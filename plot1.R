e5b4cdcd740a72b8cae7a1f015ff69d0dd61934a
mike-bennett <michael.t.bennett14@gmail.com>
2020-08-16 12:19:48 -0400

    Annotate plot1.R and plot2.R

--- plot1.R
+++ plot1.R
@@ -1,16 +1,21 @@
 library(tidyverse)
 library(lubridate)
 
+## Read in the data
 power_messy <- read_delim("household_power_consumption.txt", delim = ";", na = c("?", "NA"))
 
 power <- power_messy %>%
     mutate(Date = dmy(Date)) %>% # Correct Date column from character to date type
     filter(Date >= "2007-02-01", Date <= "2007-02-02")
 
+## Initiate graph without annotations
 with(power, hist(Global_active_power, col = "red", ann = FALSE))
+
+## Now I can annotate
 title(main = "Global Active Power",
       xlab = "Global Active Power (kilowatts)",
       ylab = "Frequency")
 
+## Copy file to .png and close the graphics device
 dev.copy(png, "plot1.png")
-dev.off()
+dev.off() 
--- plot2.R
+++ plot2.R
@@ -1,15 +1,22 @@
 library(tidyverse)
 library(lubridate)
 
+
+## Read in the data
 power_messy <- read_delim("household_power_consumption.txt", delim = ";", na = c("?", "NA"))
+
 
 power <- power_messy %>%
     mutate(Date = dmy(Date)) %>% # Correct Date column from character to date type
     filter(Date >= "2007-02-01", Date <= "2007-02-02") %>% 
-    mutate(Date_time = ymd(Date) + hms(Time))
+    mutate(Date_time = ymd(Date) + hms(Time)) ## Add new column for date and time together
 
+## Initiate graph without annotations
 with(power, plot(Date_time, Global_active_power, type = "l", ann = FALSE))
+
+## Now I can annotate
 title(ylab = "Global Active Power (kilowatts)")
 
+## Copy to .png and close graphics device
 dev.copy(png, "plot2.png")
 dev.off()
